7e2f2e2cf0c1dddc4c9a3150b75a6fa06b0fb871
jonah odoh <jonahodoh@gmail.com>
2025-01-31 19:44:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
+
+app.use(cors({
+    origin: 'localhost:5173',
+}));
+
+
+app.get('/', (req, res) => {
+    const email = 'jonahodoh@gmail.com';
+    const current_datetime = new Date().toISOString(); 
+    const github_url = 'https://github.com//hng12-api'; 
+
+    const response = {
+        email,
+        current_datetime,
+        github_url,
+    };
+
+    res.status(200).json(response);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
